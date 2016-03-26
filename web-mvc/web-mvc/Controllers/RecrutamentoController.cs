14f4e57302514ddace19c0414715a466aca45e2d
DanielCauser <danielcauser@gmail.com>
2016-03-26 15:58:14 -0300

    funcionando

---
+++ web-mvc/web-mvc/Controllers/RecrutamentoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_mvc.Models;
+
+namespace web_mvc.Controllers
+{
+    public class RecrutamentoController : Controller
+    {
+        // GET: Recrutamento
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ web-mvc/web-mvc/Models/RecrutamentoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace web_mvc.Models
+{
+    public class RecrutamentoModel
+    {
+        [Required]
+        [StringLength(30)]
+        public string Nome  { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Telefone { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Mensagem { get; set; }
+    }
+}
