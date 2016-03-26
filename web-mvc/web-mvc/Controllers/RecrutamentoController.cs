bf2fe197058062e7251217abce042270c05d7795
DanielCauser <danielcauser@gmail.com>
2016-03-26 19:07:18 -0300

    Recaptcha

--- web-mvc/web-mvc/Controllers/RecrutamentoController.cs
+++ web-mvc/web-mvc/Controllers/RecrutamentoController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using web_mvc.Models;
+using Recaptcha.Web;
+using Recaptcha.Web.Mvc;
+using System.Threading.Tasks;
+using ReCaptcha.Mvc5;
 
 namespace web_mvc.Controllers
 {
@@ -12,7 +17,68 @@ namespace web_mvc.Controllers
         // GET: Recrutamento
         public ActionResult Index()
         {
+            ViewBag.Erro = false;
+            ViewBag.Mensagem = string.Empty;
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Alistarse(RecrutamentoModel model)
+        {
+            ViewBag.Erro = false;
+            ViewBag.Mensagem = string.Empty;
+            if (ModelState.IsValid)
+            {
+                // Verify the recaptcha response.
+                ReCaptchaResponse response = await this.verifyReCAPTCHA(model, "6LfKyxsTAAAAAOWxz6s77gdm8aqoGfA0Gzq6fCgb", true);
+                if (response.Success)
+                {
+                    try
+                    {
+                        if (model.Mensagem != null && model.Email != null && model.Nome != null && model.Telefone != null)
+                        {
+
+
+                            MailMessage mail = new MailMessage("barbaroswarsite@gmail.com", "barbaroswarsite@gmail.com");
+                            SmtpClient client = new SmtpClient();
+                            client.Port = 25;
+                            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                            client.UseDefaultCredentials = false;
+                            client.Host = "smtp.gmail.com";
+                            mail.Subject = "Nova solicitação de recrutamento";
+                            mail.Body = "Olá, o clasher: " + model.Nome.ToString() + "\r\n" +
+                                        "Telefone: " + model.Telefone.ToString() + "\r\n" +
+                                        "E-mail: " + model.Email.ToString() + "\r\n" +
+                                        "Solicitou entrar no clãn assim: " + model.Mensagem.ToString() + "\r\n";
+
+                            client.EnableSsl = true;
+                            client.Credentials = new System.Net.NetworkCredential("barbaroswarsite@gmail.com", "juliajael086");
+
+                            client.Send(mail);
+
+                            ViewBag.Mensagem = "Alistamento concluido - aguarde que entraremos em contato.";
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.Erro = true;
+                        ViewBag.Mensagem = "Ooops... Ocorreu um problema, tente novamente mais tarde.";
+                    }
+
+
+                    return View("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Pare de spamar, vá ler um livro!");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View("Index");
         }
     }
 }
--- web-mvc/web-mvc/Models/RecrutamentoModel.cs
+++ web-mvc/web-mvc/Models/RecrutamentoModel.cs
@@ -4,21 +4,28 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+
 namespace web_mvc.Models
 {
-    public class RecrutamentoModel
+    public class RecrutamentoModel : ReCaptcha.Mvc5.Model.ReCaptchaViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Nome in-game")]
         [StringLength(30)]
         public string Nome  { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "E-mail obrigatório")]
         [StringLength(50)]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage="Zapzap obrigatório")]
         [StringLength(30)]
         public string Telefone { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Mensagem de recrutamento obrigatória")]
         [StringLength(250)]
         public string Mensagem { get; set; }
+
+        [Required(ErrorMessage = "Stop spammig. Read books !")]
+        public override string recaptcha_response_field { get; set; }
     }
 }
